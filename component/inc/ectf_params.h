0dc3455ddc7c6d5d201a899454a63061e8a97512
Norgalador <josh.burlile@gmail.com>
2024-02-15 13:51:47 -0800

    added insecure repo

---
+++ component/inc/ectf_params.h
@@ -0,0 +1,8 @@
+#ifndef __ECTF_PARAMS__
+#define __ECTF_PARAMS__
+#define COMPONENT_ID 2003195177
+#define COMPONENT_BOOT_MSG "C29 Boot Workshop"
+#define ATTESTATION_LOC "California"
+#define ATTESTATION_DATE "1/23/24"
+#define ATTESTATION_CUSTOMER "Josh"
+#endif
